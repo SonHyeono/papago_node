1c40edfa5833be23b81a1602d6a6c4a38ebc1166
SonHyeonoSonny <oktlrp7624583@gmail.com>
2022-02-23 15:58:31 +0900

    Update project
    파파고 관련한 프로젝트 완성! 영어로 입력시 네이버 api에 요청해서
    언어감지하고 en(영어)이면 번역해서 값을 보여준다.
    
    lease enter the commit message for your changes. Lines starting

--- server.js
+++ server.js
@@ -7,11 +7,98 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 
-const client_id = process.env.client_id
-const client_secret = process.env.CLIENT_SECRET
+const clientId = process.env.client_id
+const clientSecret = process.env.CLIENT_SECRET
 
 //node.js 서버가 또 다른 client가 되어 Naver 서버에 요청을 보내기 위해 사용.
 const request = require('request');
 
 // express의 static 미들웨어 활용.
 app.use(express.static('public'));
+
+// express의 json 미들웨어 활용.
+app.use(express.json());
+
+// console.log(`현재 파일명: ${__filename}`);
+// console.log(`index.html의 파일 경로: ${__dirname}`);
+
+// root url ( 기본 url ) : localhost:3000/ == localhost:3000
+// 해당 경로로 요청이 들어왔을 때 호출될 함수.
+// 두 인자값(arguments)을 받음 : request(req), response(res)
+app.get("/",(req, res) => {
+    // root url, 즉 메인 페이지로 접속했을 때 papago의 메인 페이지가 나와야함.
+    // public/ ~
+    res.sendFile(__dirname, 'index.html');
+});
+
+// detectLangs 경로로 요청했을 때
+app.post("/detectLangs", (req, res) => {
+    // '안녕'이라는 텍스트를 받아야함.
+
+    //JSON을 object로 파싱해줘야하는데 javascript는 알아서 해준다
+    console.log(req.body);
+    console.log(typeof req.body); // object?
+
+    const { text:query, targetLanguage } = req.body;
+    //console.log(query);
+    //console.log(targetLanguage);
+
+    const url = "https://openapi.naver.com/v1/papago/detectLangs";
+    const options = {
+        url,
+        form: { query: query },
+        headers: {
+            "X-Naver-Client-Id": clientId,
+            "X-Naver-Client-Secret": clientSecret,
+        },
+
+    }
+
+    // 언어 감지 요청 부분.
+    // options에 요청에 필요한 데이터
+    // () => {} : 요청에 따른 응답 정보를 확인
+    request.post(options, (error, response, body) => { 
+        if(!error && response.statusCode == 200){
+            const parseBody = JSON.parse(body); // parse() : string -> object로 변환
+            console.log(typeof parseBody, parseBody);
+
+            //papago 번역 url로 redirect(재요청).
+            res.redirect(`translate?lang=${parseBody['langCode']}&targetLanguage=${targetLanguage}&query=${query}`); // 쿼리 스트링(query string) -> get요청
+            // localhost:3000/translate?lang=ko&targetLanguage=en&query=안녕(%2D%G5~~, 퍼센트 인코딩)
+        }else{
+            console.log(`error = ${response.statusCode}`);
+        }
+    });
+
+});
+
+// papago 번역 요청 부분.
+app.get("/translate", (req, res) => {
+    const url = 'https://openapi.naver.com/v1/papago/n2mt';
+    console.log(req.query, typeof req.query);
+    const options = {
+        url,
+        form: {
+            source: req.query['lang'], // query string으로 받은 값들 mapping or binding.
+            target: req.query['targetLanguage'], 
+            text: req.query['query'],
+        },
+        headers: {
+            "X-Naver-Client-Id": clientId,
+            "X-Naver-Client-Secret": clientSecret,
+        },
+    }
+
+    request.post(options, (error, response, body) => {
+        if(!error && response.statusCode == 200){
+            res.json(body); // front에 해당하는 script.js에 응답 데이터(json) 전송.
+        } else {
+            console.log(`error = ${response.statusCode}`);
+        }
+
+    });
+});
+
+app.listen(3000, () => {
+    console.log('http://127.0.0.1:3000/ app listening on port 3000!');
+});
